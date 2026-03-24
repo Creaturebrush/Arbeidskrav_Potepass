e6026991a3df5b50a54380e97c5a4d9259714ea1
iTeien <fredrik.teien@gmail.com>
2026-03-24 20:37:14 +0100

    Updated modal scripts

--- src/Pages/profile/profile.ts
+++ src/Pages/profile/profile.ts
@@ -1,3 +1,26 @@
 // MODALS
 
+const modal = document.querySelector(".modal-wrapper") as HTMLTemplateElement;
 
+function open() {
+    modal.classList.add("open");
+    
+    if (modal.classList = "open" + )
+    modal.innerHTML = `
+    <div class="profile-modal">
+          <h2>Er du sikker på at du ønsker å slette profilen din?</h2>
+          <div class="btn-container">
+            <button class="btn btn-success" id="close-btn">NEI, GÅ TILBAKE</button>
+            <button class="btn btn-danger">JA, SLETT PROFIL</button>
+          </div>
+        </div>
+    `;
+
+    const closeBtn = document.getElementById("close-btn") as HTMLButtonElement;
+      closeBtn.addEventListener('click', () => {
+        modal.classList.remove('open');
+});
+};
+
+const deleteProfileBtn = document.getElementById("delete-profile-btn") as HTMLButtonElement;
+deleteProfileBtn.addEventListener("click", open);
