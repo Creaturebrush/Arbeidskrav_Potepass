1b7b3af2e06ef04278282284b622e93746d561be
iTeien <fredrik.teien@gmail.com>
2026-03-31 02:01:33 +0200

    updated script

--- src/Pages/profile/profile.ts
+++ src/Pages/profile/profile.ts
@@ -85,3 +85,99 @@ editProfileBtn.addEventListener("click", () => {
   `;
   modal.openModal(dynamicContent);
 });
+
+const addDogBtn = document.getElementById("add-dog-btn") as HTMLButtonElement;
+addDogBtn.addEventListener("click", () => {
+  const dynamicContent = `
+          <h2>Legg til hund</h2>
+          <div class="add-edit-modal-card">
+            <div class="add-edit-img-container">
+              <img src="/images/dogicon.png" alt="Bilde av hund" />
+              <button class="btn btn-success">LAST OPP BILDE</button>
+            </div>
+            <div class="add-edit-info">
+              <p>Navn:</p>
+              <p>Rase:</p>
+              <p>Alder:</p>
+              <p>Allergier:</p>
+            </div>
+            <div class="add-edit-user-info">
+              <p>tomt felt</p>
+              <p>tomt felt</p>
+              <p>tomt felt</p>
+              <p>tomt felt</p>
+            </div>
+            <div class="add-edit-btn-container">
+              <button class="btn btn-warning">REDIGER</button>
+              <button class="btn btn-warning">REDIGER</button>
+              <button class="btn btn-warning">REDIGER</button>
+              <button class="btn btn-warning">REDIGER</button>
+            </div>
+          </div>
+          <div class="btn-container">
+            <button class="btn btn-success">LEGG TIL HUND</button>
+            <button class="btn btn-danger" id="close-btn">AVBRYT</button>
+          </div>
+  `;
+  modal.openModal(dynamicContent);
+});
+
+const removeDogBtn = document.getElementById(
+  "remove-dog-btn",
+) as HTMLButtonElement;
+removeDogBtn.addEventListener("click", () => {
+  const dynamicContent = `
+          <h2>Hvilken hund ønsker du å fjerne?</h2>
+          <div class="remove-dog-container">
+            <div class="remove-dog-card">
+              <img src="/images/dog1.png" alt="Bilde av hund" />
+              <p>NAVN</p>
+            </div>
+            <div class="remove-dog-card">
+              <img src="/images/dog1.png" alt="Bilde av hund" />
+              <p>NAVN</p>
+            </div>
+          </div>
+          <div class="btn-container">
+            <button class="btn btn-success" id="close-btn">AVBRYT</button>
+            <button class="btn btn-danger">FJERN VALGT HUND</button>
+          </div>
+  `;
+  modal.openModal(dynamicContent);
+});
+
+const editDogBtn = document.getElementById("edit-dog-btn") as HTMLButtonElement;
+editDogBtn.addEventListener("click", () => {
+  const dynamicContent = `
+          <h2>Hva ønsker du å redigere?</h2>
+          <div class="add-edit-modal-card">
+            <div class="add-edit-img-container">
+              <img src="/images/dogicon.png" alt="Bilde av hund" />
+              <button class="btn btn-success">LAST OPP BILDE</button>
+            </div>
+            <div class="add-edit-info">
+              <p>Navn:</p>
+              <p>Rase:</p>
+              <p>Alder:</p>
+              <p>Allergier:</p>
+            </div>
+            <div class="add-edit-user-info">
+              <p>tomt felt</p>
+              <p>tomt felt</p>
+              <p>tomt felt</p>
+              <p>tomt felt</p>
+            </div>
+            <div class="add-edit-btn-container">
+              <button class="btn btn-warning">REDIGER</button>
+              <button class="btn btn-warning">REDIGER</button>
+              <button class="btn btn-warning">REDIGER</button>
+              <button class="btn btn-warning">REDIGER</button>
+            </div>
+          </div>
+          <div class="btn-container">
+            <button class="btn btn-success">BEKREFT ENDRINGER</button>
+            <button class="btn btn-danger" id="close-btn">AVBRYT ENDRINGER</button>
+          </div>
+  `;
+  modal.openModal(dynamicContent);
+});
