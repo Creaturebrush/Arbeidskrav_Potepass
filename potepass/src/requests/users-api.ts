74cdb50e80504f14d7def40bca829ea599c02ad4
Creaturebrush <a-bra@hotmail.com>
2026-04-12 22:17:18 +0200

    added crudops, fixed folders, added get request and user type and used it to change username on booking site

---
+++ potepass/src/requests/users-api.ts
@@ -0,0 +1,19 @@
+import { type User } from "../types/user.type";
+
+export async function getAllUsers(): Promise<User[]>{
+    try{
+        const response: Response = await fetch (
+            "http://localhost:3000/api/users",
+        );
+
+        if (!response.ok){
+            throw new Error("Kunne ikke hente data" + response.status);
+        }
+
+        const data: User[] = await response.json();
+        return data;
+    } catch(error){
+        console.log("Oops, noe gikk galt" + error);
+        throw error; 
+    }
+}
