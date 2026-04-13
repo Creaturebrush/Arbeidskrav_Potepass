308222dc41ace539d4cb656da68ac383fd5075e9
Creaturebrush <a-bra@hotmail.com>
2026-04-13 17:53:06 +0200

    added crudops

--- potepass/src/Pages/booking/booking.ts
+++ potepass/src/Pages/booking/booking.ts
@@ -1,19 +1,19 @@
 import { type User } from "../../types/user.type";
 import { getAllUsers } from "../../requests/users-api";
 
+async function getUsers() {
+  const users: User[] = await getAllUsers();
 
-async function getUsers(){
-    const users: User[] = await getAllUsers();
-
-    //Sender videre variabelen users. 
-    showUserName(users)
+  //Sender videre variabelen users.
+  showUserName(users);
 }
 
-getUsers()
-
+getUsers();
 
 //Variabelen users hentes fra funksjonen getUsers slik at den kan brukes i funksjonen under.
-function showUserName(users: User[]){
-    const dogSitterName = document.querySelector(".dog-sitter-name") as HTMLHeadingElement;
-    dogSitterName.innerHTML = users[0].userName;
+function showUserName(users: User[]) {
+  const dogSitterName = document.querySelector(
+    ".dog-sitter-name",
+  ) as HTMLHeadingElement;
+  dogSitterName.innerHTML = users[0].userName;
 }
